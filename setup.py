1733da2a0fea1e0522aad18ab1352abdc9edb434
Nathan Osman <nathan@quickmediasolutions.com>
2016-03-15 17:37:23 -0700

    Corrected package name.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='multi_fk',
+    name='django-multi-fk',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
